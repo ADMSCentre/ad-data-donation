2260b54304dcf3f3253eb037fb791dd5e0a86b4f
Dan Tran <d.k.tran@uq.edu.au>
2024-09-19 16:02:14 +1000

    feat: Update page to include link to GitHub

--- src/framework/visualisation/react/main.tsx
+++ src/framework/visualisation/react/main.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import AuthProvider, { AuthContext } from "./contexts/AuthContext";
-import { BsQuestionCircle } from "react-icons/bs"
+import { BsQuestionCircle, BsGithub } from "react-icons/bs"
 import { Tooltip } from 'react-tooltip'
 import NavBar from "./ui/elements/navbar";
 import { Login, Logout } from "./ui/elements/authentication";
@@ -28,12 +28,12 @@ function Logo() {
   const orgs = [
     {
       name: "ARC Centre of Excellence for Automated Decision-Making and Society",
-      logoUrl: "https://www.unimelb.edu.au/__data/assets/image/0006/3625854/ADM-S-Logo.png",
+      logoUrl: "images/logos/adms.png",
       url: "https://www.admscentre.org.au/"
     },
     {
       name: "University of Queensland",
-      logoUrl: "https://usercontent.one/wp/studyoptions.com/wp-content/uploads/2023/08/UQlogo.png",
+      logoUrl: "images/logos/uq.png",
       url: "https://www.uq.edu.au/"
     }
   ]
@@ -44,11 +44,11 @@ function Logo() {
         <a key={index} href={org.url}
           target="_blank"
           rel="noreferrer"
-          className="h-full p-[1px] border-primary border-opacity-0 border-2 hover:border-opacity-100 transition-all"
+          className="h-full p-[1px] border-text border-opacity-0 border-2 hover:border-opacity-100 transition-all"
         >
           <img
             src={org.logoUrl} alt={org.name}
-            className="!m-0 bg-white object-contain max-h-full"
+            className="!m-0 object-contain max-h-full"
           />
         </a>
       ))}
@@ -68,7 +68,7 @@ const Standalone = ({ elements }: MainProps): JSX.Element => {
 
   return (
     <>
-      <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col w-full h-full min-h-screen">
         <header className="sm:sticky top-0 z-[999] flex-col flex md:flex-row justify-between sm:items-center h-full border-primary p-2 bg-primary gap-2">
           <div className="flex gap-2 items-center flex-col sm:flex-row">
             <Logo />
@@ -80,9 +80,27 @@ const Standalone = ({ elements }: MainProps): JSX.Element => {
               : <Login />}
           </div>
         </header>
-        <div className="p-4 sm:p-8 md:p-12 flex justify-center w-full h-full">
+        <div className="p-4 flex-1 sm:p-8 md:p-12 flex justify-center w-full h-full">
           <div>{elements}</div>
         </div>
+        <footer className="flex flex-col justify-center text-text px-8 py-4 gap-4 min-h-10 bg-primary">
+          <div className="flex flex-row justify-between items-center ">
+            <div>Adapted from <a href="https://github.com/d3i-infra/data-donation-task" target="_blank">d3i-infra/data-donation-task</a></div>
+            <div className='flex justify-center gap-4'>
+              {/* Logos */}
+              <img src="images/logos/adms.png" alt="ARC Centre of Excellence for Automated Decision-Making and Society" className="h-12 !m-0" />
+              <img src="images/logos/uq.png" alt="University of Queensland" className="h-12 !m-0" />
+              <img src="images/logos/qut.png" alt="Queensland University of Technology" className="h-12 !m-0" />
+            </div>
+            <div className="flex gap-2 items-center">
+              Find us on
+              <a href="https://github.com/ADMSCentre/ad-data-donation" target="_blank">
+                <BsGithub size={28} />
+              </a>
+            </div>
+          </div>
+
+        </footer>
       </div>
       <Tooltip id="my-tooltip" style={{
         maxWidth: "500px",
